78c46b2669777a3a7c32d079d7ef66a56f8797f7
grabstacker <bradkay12@gmail.com>
2019-08-10 00:56:13 +1200

    added functionality to retrive and post tweets depending on custom search criteria

--- TwitterApp/index.js
+++ TwitterApp/index.js
@@ -6,11 +6,13 @@ var T = new Twit(config)
   
 console.log(config)
 
+//This function gets a certain topic and runs a loop of the amount of top tweets to post 
+//to the twitter account. It also posts to log to show what was posted locally
 T.get('search/tweets', { q: 'bitcoin, craig wright since:2011-07-11', count: 100 }, function(err, data, response) {
     
     var tweets = data.statuses
     var postTweet
-    for (let i = 0; i < 1; i++){
+    for (let i = 0; i < 1; i++){ // <-- adjust this loop to control the amount of tweets to post
      postTweet = tweets[i].text
     console.log(tweets[i].text)
     }
