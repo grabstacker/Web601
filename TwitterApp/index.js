a705bbf114537936c6e70f0c615b741de67360e7
grabstacker <bradkay12@gmail.com>
2019-08-10 14:29:45 +1200

    Created an interval and function to tweet every 20 seconds latest Bitcoin news

--- TwitterApp/index.js
+++ TwitterApp/index.js
@@ -4,24 +4,34 @@ var Twit = require('twit')
 var config = require('./config')
 var T = new Twit(config)
   
-console.log(config)
+//console.log(config)
 
 //This function gets a certain topic and runs a loop of the amount of top tweets to post 
 //to the twitter account. It also posts to log to show what was posted locally
-T.get('search/tweets', { q: 'bitcoin, craig wright since:2011-07-11', count: 100 }, function(err, data, response) {
+function tweetIT(){
+T.get('search/tweets', { q: 'bitcoin, since:2011-07-11', count: 100 }, function(err, data, response) {
     
     var tweets = data.statuses
     var postTweet
     for (let i = 0; i < 1; i++){ // <-- adjust this loop to control the amount of tweets to post
      postTweet = tweets[i].text
-    console.log(postTweet)
+   
     }
 
 // posts the retrieved tweets to the twitter account
 //Can put inside the for loop to send out multiple tweets but i think that might break it    
+
     T.post('statuses/update', { status: ' ' + postTweet }, function(err, data, response) {
-        console.log(data)
+        if(err){
+            console.log("\nSomthing went wrong! : " + err + '\n')
+        }else{
+            console.log("\nIt worked Tweet has been sent: \n\n")
+        }
+        console.log(postTweet)
       })
+
 })
 
-
+}
+setInterval(tweetIT, 1000*20) //sets the interval for searching and posting the tweet
+tweetIT();
