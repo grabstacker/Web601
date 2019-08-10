96fd062fda1ddb223a36af72ac9f03d050282b7d
grabstacker <bradkay12@gmail.com>
2019-08-10 16:10:12 +1200

    Added some features of repost Tweets and follow the user that the tweet was taken from

--- TwitterApp/index.js
+++ TwitterApp/index.js
@@ -3,35 +3,47 @@ var Twit = require('twit')
 
 var config = require('./config')
 var T = new Twit(config)
-  
+
 //console.log(config)
 
 //This function gets a certain topic and runs a loop of the amount of top tweets to post 
 //to the twitter account. It also posts to log to show what was posted locally
-function tweetIT(){
-T.get('search/tweets', { q: 'bitcoin, since:2011-07-11', count: 100 }, function(err, data, response) {
-    
-    var tweets = data.statuses
-    var postTweet
-    for (let i = 0; i < 1; i++){ // <-- adjust this loop to control the amount of tweets to post
-     postTweet = tweets[i].text
-   
-    }
+function tweetIT() {
+    T.get('search/tweets', { q: 'bitcoin, since:2011-07-11', count: 100 }, function (err, data, response) {
 
-// posts the retrieved tweets to the twitter account
-//Can put inside the for loop to send out multiple tweets but i think that might break it    
-
-    T.post('statuses/update', { status: ' ' + postTweet }, function(err, data, response) {
-        if(err){
-            console.log("\nSomthing went wrong! : " + err + '\n')
-        }else{
-            console.log("\nIt worked Tweet has been sent: \n\n")
+        var tweets = data.statuses
+        var postTweet
+        var screen_name
+        for (let i = 0; i < 1; i++) { // <-- adjust this loop to control the amount of tweets to post
+            postTweet = tweets[i].text
+            screen_name = data.statuses[i].user.screen_name;
         }
-        console.log(postTweet)
-      })
+        //Automatically follows the user that the tweet is sourced from
 
-})
 
+
+        T.post('friendships/create', { screen_name }, function (err, res) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(screen_name, ': **FOLLOWED**');
+            }
+        })
+
+        //posts the tweet that is T.get
+        T.post('statuses/update', { status: ' ' + postTweet }, function (err, data, response) {
+            if (err) {
+                console.log("\nSomthing went wrong! : " + err + '\n')
+            } else {
+                console.log("\nIt worked Tweet has been sent: \n\n")
+            }
+            console.log(postTweet)
+        })
+    }
+    )
 }
-setInterval(tweetIT, 1000*20) //sets the interval for searching and posting the tweet
-tweetIT();
+
+
+
+setInterval(tweetIT, 1000 * 60) //sets the interval for searching and posting the tweet
+tweetIT()
