81f4a0acf3f83b9143411038f0c39161dc36b209
grabstacker <bradkay12@gmail.com>
2019-08-13 01:48:21 +1200

    Tested some simple logic in Javscript/ Alternating button text

--- htmlPrac.js
+++ htmlPrac.js
@@ -1,1 +1,12 @@
 document.getElementById("p1").innerHTML = "working!";
+var change = true
+function msg(){
+   
+    if (change == true){
+        change = false;
+    document.getElementById("p1").innerHTML = "changed to false";
+    }else{
+        document.getElementById("p1").innerHTML = "changed to true";
+        change = true
+    }
+}
