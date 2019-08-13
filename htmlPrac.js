1e08c4ec8a6b641f17c11f9df303d57cc8f9cb8a
grabstacker <bradkay12@gmail.com>
2019-08-13 18:09:31 +1200

    added some canvas html tags and changed the colour with a for loop in js

--- htmlPrac.js
+++ htmlPrac.js
@@ -1,17 +1,38 @@
 document.getElementById("p1").innerHTML = "working!";
 var change = true
-function msg(){
-   
-    if (change == true){
+
+function msg() {
+
+    if (change == true) {
         change = false;
-    document.getElementById("p1").innerHTML = "changed to false";
-    }else{
+        document.getElementById("p1").innerHTML = "changed to false";
+    } else {
         document.getElementById("p1").innerHTML = "changed to true";
         change = true
     }
 
     for (let index = 0; index < 10; index++) {
-       document.getElementById("p2").innerHTML += "</br> Loop number " + index + "\n";
-        
+        document.getElementById("p2").innerHTML += "</br> Loop number " + index + "\n";
+
     }
+
+
+}
+
+function changeColours() {
+    for (let i = 0; i < 12; i++) {
+
+
+
+        const canvas = document.getElementById("newCanvas" + i);
+        var ctx = canvas.getContext('2d');
+        var num = Math.floor(Math.random() * 200)
+
+        ctx.fillStyle = 'rgb(' + num + ',' +
+            Math.floor(Math.random() * 300) + ', ' +
+            Math.floor(Math.random() * 300) + '';
+
+        ctx.fillRect(0, 0, 300, 300);
+    }
+
 }
