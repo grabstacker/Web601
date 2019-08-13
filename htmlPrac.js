4eca36fb0c974633ddd84619546164a4df4266b3
grabstacker <bradkay12@gmail.com>
2019-08-13 18:11:52 +1200

    commenting and formating

--- htmlPrac.js
+++ htmlPrac.js
@@ -1,6 +1,7 @@
 document.getElementById("p1").innerHTML = "working!";
 var change = true
 
+//changes some text and interacts with the button
 function msg() {
 
     if (change == true) {
@@ -19,10 +20,9 @@ function msg() {
 
 }
 
+//changes the background colour of the canvases on html doc
 function changeColours() {
     for (let i = 0; i < 12; i++) {
-
-
 
         const canvas = document.getElementById("newCanvas" + i);
         var ctx = canvas.getContext('2d');
